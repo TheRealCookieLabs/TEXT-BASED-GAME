e174d08f83dd29ca662c8636eae78d3f884bcf66
ckonjeti <ckonjeti1@gmail.com>
2016-02-14 00:18:18 -0600

    ok

--- MainActual.py
+++ MainActual.py
@@ -33,7 +33,7 @@ def start():
     prompt_wrapchains()
     print('')
     print ('''If you\'re curious, you can explore the facility(1). You can refill your health by finding food(2), but who knows what else you might find.
-If you wanna play it safe, you could search for a phone and call 911(3).''')
+If you wanna play it safe, you could search for a phone and call 911(3).''')    
     prompt_survival()
     print('')
     if(prompt_0 == '1'):
@@ -248,22 +248,10 @@ def prompt_attackzombie():
             print("You feel adrenaline rush through your veins.")
             time.sleep(2)
             print("You have the option of running away(1) or continue fighting the zombie with new strength(2).")
-            prompt_adrenalinerush()
 
-def prompt_adrenalinerush
-    global health
-    global zombieHealth
-    global meleeAttack
-    global kickAttack
-    global zombieScratchAttack
-    prompt_0 = input("Type your choice: ")
-    if prompt_0 == '1':
-        print("You get up and run down the hallway, with the zombie following behind you.)
-        time.sleep(2)
-        print("When you get to the end of the hallway, you see a locker(1) that you could jump into to hide, or you can keep running(2).)      
-        prompt_hiderun()
-    if prompt_0 == '2':
-              
+            
+            
+        
     
         
     
